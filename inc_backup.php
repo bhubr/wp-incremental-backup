c27321bc28557c8e615c8d22e9dbba14c8ece021
Little Buddha <littlebuddha31@gmail.com>
2016-06-09 01:29:07 +0200

    find new, modified, deleted files

--- inc_backup.php
+++ inc_backup.php
@@ -7,6 +7,12 @@ function regexize_path($path) {
 
 class Md5Walker {
 
+	private $walk_dir;
+	private $cnt;
+	private $output_file;
+	private $first_run;
+	private $files;
+
 	/**
 	 * Initialize walk_dir, count, csv file
 	 */	
@@ -14,6 +20,13 @@ class Md5Walker {
 		$this->walk_dir = $walk_dir;
 		$this->cnt = 0;
 		$this->output_file = __DIR__ . "/list.csv";
+		$this->first_run = !file_exists($this->output_file);
+		if ($this->first_run) {
+			$this->files = [];
+		}
+		else {
+			$this->read();
+		}
 	}
 
 	/**
@@ -46,6 +59,7 @@ class Md5Walker {
 		$md5 = md5_file($name);
 		// echo "$name $md5\n";
 		fwrite($this->fh, $this->line($name, $md5));
+		return $md5;
 	}
 
 	/**
@@ -63,19 +77,28 @@ class Md5Walker {
 				throw new Exception("invalid handle, aborting");
 			}
 			
-			var_dump($line_read);
+			$name = $line_read[0];
+			$md5 = $line_read[1];
+			$this->files[$name] = $md5;
 		} while($line_read !== false);
-		echo "end of file\n";
+		// echo "end of file\n";
+	}
+
+	public function get_md5($name) {
+		return $this->files[$name];
 	}
 
 	public function walk() {
+		$found_in_dirs = [];
 		$this->fh = fopen($this->output_file, "w");
 
 		$objects = new RecursiveIteratorIterator(
 			new RecursiveDirectoryIterator($this->walk_dir),
 			RecursiveIteratorIterator::SELF_FIRST
 		);
-		foreach($objects as $name => $object){
+
+		// Iterate directory
+		foreach($objects as $name => $object) {
 			
 			// Skip if this is . or ..
 			if($this->is_special_dir($object)) {
@@ -87,62 +110,25 @@ class Md5Walker {
 				$this->add_dir($name);
 				continue;
 			}
+			$found_in_dirs[] = $name;
 
-			$this->add_file($name);
-			// if($this->is_regular_file($object)) {
-			// 	echo "$name\n";
-			// 	$this->cnt++;
-			// }
-			// var_dump($object);
-			// if(!is_file($name)) {
-			// 	echo "skip $name\n";
-			// 	continue;
-			// }
-			// $path = dirname($name);
-			// $file_name = basename($name);
-			// $file_size = filesize($name);
-			// if ($file_size < $this->min_size) continue;
+			$md5 = $this->add_file($name);
+			if($this->first_run || !array_key_exists($name, $this->files)) {
+				echo "new file: $name\n";
+				continue;
+			}
+			$old_md5 = $this->get_md5($name);
+			if($md5 !== $old_md5) {
+				echo "modified file: $name (old md5 = $old_md5, new md5 = $md5)\n";
+			}
 
+		}
+		// var_dump($found_in_dirs);
 
-			// echo "$name,$md5_sum\n";
-			// $query = sprintf("SELECT file_name,md5_sum FROM files WHERE file_name='%s' AND md5_sum = '%s'", $file_name, $md5_sum);
-			// try {
-			// 	// echo "1...";
-			// 	// $stmt = $this->db->prepare("SELECT file_name,md5_sum FROM files WHERE file_name=? AND md5_sum = ?");
-			// 	$res = $this->db->query($query);
-			// 	// echo "2...";
-			// 	// $stmt->bind_param('ss', $file_name, $md5_sum);
-			// 	// , [$file_name, $md5_sum]);
-			// 	// echo "3...";
-			// 	// $stmt->execute();
-			// 	// echo "4...";
-			// 	// $res = $stmt->get_result();
-			// 	// echo "5...";
-			// } catch(Exception $e) {
-			// 	echo "exception catched...";
-			// 	die($e->getMessage());
-			// }
-			// // var_dump($res);
-			// // var_dump($res->num_rows);
-			// // echo "6...";
-			// if (!$res || $res->num_rows === 0) {
-			// 	$query = sprintf(
-			// 		"INSERT INTO files(host, volume, path, file_name, size, md5_sum, created_at) VALUES('%s', '%s', '%s', '%s', '%s', '%s', NOW())",
-			// 		$this->host, $this->volume, $path, $file_name, $file_size, $md5_sum
-			// 	);
-			// 	// var_dump($query);
-			// 	$res = $this->db->query($query);
-			// 	$this->cnt++;
-
-			// 	// $stmt = $this->db->prepare("INSERT INTO files(host, volume, path, file_name, size, md5_sum, created_at) VALUES(?, ?, ?, ?, ?, ?, NOW())");
-			// 	// $stmt->bind_param('ssssss', $this->host, $this->volume, $path, $file_name, $file_size, $md5_sum);
-			// 	// $stmt->execute();
-			// 	// $res = $stmt->get_result();
-			// 	// var_dump($res->num_rows);
-			// }
-			
-			// 	// printf("%s %s\n", dirname($name), basename($name));
-		    
+		foreach($this->files as $name => $md5) {
+			if (!empty($md5) && array_search($name, $found_in_dirs) === false) {
+				echo "deleted file: $name\n";
+			}
 		}
 
 		fclose($this->fh);
@@ -157,4 +143,4 @@ $args = !isset($argv) ? [ 'root' => $_GET['root'] ] :
 $walker = new Md5Walker($args['root']);
 
 $walker->walk();
-$walker->read();
+// $walker->read();
