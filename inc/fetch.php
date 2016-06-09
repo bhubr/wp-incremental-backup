1beed4ef2f51544b045b328f7f5f4e547b41efa1
Little Buddha <littlebuddha31@gmail.com>
2016-06-09 15:27:11 +0200

    add .sql.zip as media (commented out)

--- inc/fetch.php
+++ inc/fetch.php
@@ -9,7 +9,7 @@ $cookie = tempnam ("/tmp", "CURLCOOKIE");
 
 // 1- GET
 $ch = curl_init();
-curl_setopt ($ch, CURLOPT_URL, $url . "maitreyoda/");
+curl_setopt ($ch, CURLOPT_URL, $url . "tirelipinpon/");
 curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 curl_setopt ($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6");
 curl_setopt ($ch, CURLOPT_TIMEOUT, 60);
