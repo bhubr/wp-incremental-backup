f7be845906025be56ceb3234e2c6dde64fe13030
Benoît Hubert <benoithubert@gmail.com>
2016-06-15 23:24:23 +0200

    Add exception handling

--- tasks/fallback_zip.php
+++ tasks/fallback_zip.php
@@ -1,4 +1,5 @@
 <?php
+echo "php manual zip build\n";
 if (php_sapi_name() !== 'cli') {
     $protocol = (isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0');
     $error = '404 Not Found';
@@ -13,8 +14,13 @@ $filename = "{$output_fullpath_prefix}.zip";
 if ($zip->open($filename, ZipArchive::CREATE) !== true) {
     throw new T1z_WPIB_Exception("Could not open ZIP archive $filename\n", T1z_WPIB_Exception::ZIP);
 }
-$zip->addFile("{$output_fullpath_prefix}.sql","{$output_file_prefix}.sql");
-if (file_exists("{$output_fullpath_prefix}.tar")) {
-    $zip->addFile("{$output_fullpath_prefix}.tar","{$output_file_prefix}.tar");
+try {
+	$zip->addFile("{$output_fullpath_prefix}.sql","{$output_file_prefix}.sql");
+	if (file_exists("{$output_fullpath_prefix}.tar")) {
+	    $zip->addFile("{$output_fullpath_prefix}.tar","{$output_file_prefix}.tar");
+	}
+	$zip->close();
+
+} catch(Exception $e) {
+	die("Error while creating ZIP: $filename");
 }
-$zip->close();
