c8dfeff6405b726224599e29d3e31022e0656774
Benoît Hubert <benoithubert@gmail.com>
2016-06-15 23:25:14 +0200

    Pass excluded flag to md5 walker class

--- tasks/run_walker_md5_csv.php
+++ tasks/run_walker_md5_csv.php
@@ -1,6 +1,7 @@
 <?php
 define('PLUGIN_DIR', realpath(__DIR__ . '/..'));
 require PLUGIN_DIR . '/class-t1z-incremental-backup-md5-walker.php';
+
 if (php_sapi_name() !== 'cli') {
 	$protocol = (isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0');
 	$error = '404 Not Found';
@@ -11,6 +12,7 @@ if($argc < 2) exit;
 $output_csv = $argv[1];
 $archive_list = $argv[2];
 $input_dir = $argv[3];
+$excluded = ($argc > 4) ? explode(',', $argv[4]) : [];
 
-$walker = new T1z_Incremental_Backup_MD5_Walker($output_csv, $archive_list, $input_dir);
+$walker = new T1z_Incremental_Backup_MD5_Walker($output_csv, $archive_list, $input_dir, $excluded);
 $walker->prepare_files_archive();
