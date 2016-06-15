ae4314e2e1f09b3ca32eecd018ad75f7fd536d6b
Little Buddha <littlebuddha31@gmail.com>
2016-06-15 23:23:39 +0200

    Move generated unpacked plugin distribution to dist/

--- make-dist.sh
+++ make-dist.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 mkdir -p wp-incremental-backup/vendor
 mv wp-incremental-backup.zip wp-incremental-backup.zip.bak
-cp -R class-t1z-* common composer.* forms tasks trait-t1z-walker-common.php wp-incremental-backup.php wp-incremental-backup/
+cp -R class-t1z-* common composer.* download-script.php forms tasks trait-t1z-walker-common.php wp-incremental-backup.php wp-incremental-backup/
 cp -R vendor/autoload.php wp-incremental-backup/vendor/
 cp -R vendor/composer wp-incremental-backup/vendor/
 cp -R vendor/ifsnop wp-incremental-backup/vendor/
 cp -R vendor/diversen wp-incremental-backup/vendor/
 zip -r wp-incremental-backup.zip wp-incremental-backup/
-rm -r wp-incremental-backup
+rm -r dist/wp-incremental-backup
+mv wp-incremental-backup dist/
